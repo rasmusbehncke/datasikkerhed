56482ff94167d66c8bd1bfbaa0837e11c91145d7
rasmusbehncke <145353938+rasmusbehncke@users.noreply.github.com>
2024-04-18 16:04:01 +0200

    Update uden JS i HTML

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //skygge på box-knapper
 
+// Hent referencer til alle box-elementerne
+var boxes = document.querySelectorAll('.box');
+
+// Tilføj event listeners til hver box
+boxes.forEach(function(box) {
+  box.addEventListener('mouseover', addShadow);
+  box.addEventListener('mouseout', removeShadow);
+});
+
+// Funktion til at tilføje skygge
+function addShadow() {
+  this.classList.add('shadow');
+}
+
+// Funktion til at fjerne skygge
+function removeShadow() {
+  this.classList.remove('shadow');
+}
+
+
+
+/* tidligere version
+
 function addShadow(element) {
     element.classList.add('shadow');
 }
@@ -8,3 +31,4 @@ function removeShadow(element) {
     element.classList.remove('shadow');
 }
 
+*/
