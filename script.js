1d144d5da2f68d90125aff2dbf89b4251a27f773
rasmusbehncke <145353938+rasmusbehncke@users.noreply.github.com>
2024-05-01 15:56:14 +0200

    Manus done

--- script.js
+++ script.js
@@ -68,81 +68,96 @@ function nextQuestion(answer) {
   // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
   if (answer === 'answer1') {
     questionDiv.innerHTML = `
-      <h2>Nej - lav unikt</h2>
-      <p>Hvordan laver du et unikt password?:</p>
-      <a href="#" id="nytSvar1" class="box">Minimumskrav</a>
-      <a href="#" id="nytSvar2" class="box">Minimum 15 tegn etc</a>
+      <h2>Genbrug? Nej tak!</h2>
+      <p>Det er nemt at komme til at genbruge sine passwords igen og igen. Der er mange specifikke krav til passwords og det kan virke uoverskueligt at finde på nye komplicerede koder - og også huske dem igen. 
+      </p><p>
+      Der er hjælp at få, men der er desværre ingen vej uden om - dit password skal være unikt. Hvordan vil du skabe dit nye password?:</p>
+      <a href="#" id="nytSvar1" class="box">Jeg danner et password der lige præcis opfylder sidens minimumskrav - ofte 8 tegn.</a>
+      <br><br>
+      <a href="#" id="nytSvar2" class="box">Jeg danner et password på mindst 15 tegn med en blanding af små og store bogstaver, tal og tegn.</a>
     `;
   } else if (answer === 'answer2') {
     questionDiv.innerHTML = `
-      <h2>En ny version af et gammelt password er skidt</h2>
-      <p>Dan i stedet et nyt. Hvordan? </p>
-      <a href="#" id="nytSvar1" class="box">Minimumskrav - for memory</a>
-      <a href="#" id="nytSvar2" class="box">Minimum 15 tegn etc.</a>
+      <h2>Password 2.0</h2>
+      <p>Undersøgelser viser at mange opdaterer et gammelt password i stedet for at skifte til et nyt. Ofte bliver et årstal eller en årstid blot tilføjet. 
+      </p><p>
+      Dette er en dårlig vane. Hackere er også bekendt med denne vane. Hvis dit gamle password er blevet lækket, er det meget nemt at finde frem til den opdaterede version af dit gamle password.</p>
+      <p>
+      Der er hjælp at få, men der er desværre ingen vej uden om - dit password skal være unikt. Hvordan vil du skabe dit nye password?:</p>
+      <a href="#" id="nytSvar1" class="box">Jeg danner et password der lige præcis opfylder sidens minimumskrav - ofte 8 tegn.</a>
+      <br><br>
+      <a href="#" id="nytSvar2" class="box">Jeg danner et password på mindst 15 tegn med en blanding af små og store bogstaver, tal og tegn.</a>
     `;
   } else if (answer === 'answer3') {
     questionDiv.innerHTML = `
-      <h2>Super - det er vigtigt at have unikke passwords til forskellige platforme.</h2>
+      <h2>Sådan - det er nemlig vigtigt at have unikke passwords til forskellige platforme.</h2>
       <p>Hvordan skaber du dit nye password?</p>
-      <a href="#" id="nytSvar1" class="box">Minimumskrav</a>
-      <a href="#" id="nytSvar2" class="box">Minimum 15 tegn etc.</a>
+      <a href="#" id="nytSvar1" class="box">Jeg danner et password der lige præcis opfylder sidens minimumskrav - ofte 8 tegn.</a>
+      <br><br>
+      <a href="#" id="nytSvar2" class="box">Jeg danner et password på mindst 15 tegn med en blanding af små og store bogstaver, tal og tegn.</a>
     `;
   } else if (answer === 'nytSvar1') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
-      <h2>Yderligere spørgsmål 1</h2>
-      <p>Det er ok, men ikke det mest sikre valg</p>
+      <h2>Lidt har også ret</h2>
+      <p>Det er naturligvis okay blot at opfylde hjemmesidens krav til et password. Langt de fleste hjemmesider kræver et paswword på mindst 8 tegn - og derfor findes der også flest passwords med netop det antal karakterer. Det gør denne længde passwords mere sårbare overfor hackere. Overvej derfor om du kan lave et længere password. Center for Datasikkerhed anbefaler 15 tegn.</p>
       <a href="#" id="nytSvar3" class="box">Videre</a>
     `;
   } else if (answer === 'nytSvar2') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
       <h2>Læk!</h2>
-      <p>Dit sikre password er blevet lækket. Hvordan forholder du dig?</p>
+      <p>Dit nye og sikre password er blevet lækket. Du skal derfor på den igen - lave endnu et nyt password. Hvordan forholder du dig?</p>
       <a href="#" id="nytSvar4" class="box">Jeg genbruger mit unikke password men tilføjer noget ekstra, såsom årstal. Det er nemmere at huske.</a>
-      <a href="#" id="nytSvar5" class="box">Jeg laver et helt nyt unikt password</a>
+      <br><br>
+      <a href="#" id="nytSvar5" class="box">Jeg laver et helt nyt unikt password igen.</a>
     `;
   } else if (answer === 'nytSvar3') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
       <h2>Læk!</h2>
-      <p>Dit sikre password er blevet lækket. Hvordan forholder du dig?</p>
+      <p>Dit nye og sikre password er blevet lækket. Du skal derfor på den igen - lave endnu et nyt password. Hvordan forholder du dig?</p>
       <a href="#" id="nytSvar4" class="box">Jeg genbruger mit unikke password men tilføjer noget ekstra, såsom årstal. Det er nemmere at huske.</a>
-      <a href="#" id="nytSvar5" class="box">Jeg laver et helt nyt unikt password</a>
+      <br><br>
+      <a href="#" id="nytSvar5" class="box">Jeg laver et helt nyt unikt password igen.</a>
     `;
   } else if (answer === 'nytSvar4') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
-      <h2>Ikke Genbrug</h2>
-      <p>ikke genbrug bla bla lav unikt </p>
+      <h2>Genbrug? Nej tak!</h2>
+      <p>Det er nemt at komme til at genbruge sine passwords igen og igen. Der er mange specifikke krav til passwords og det kan virke uoverskueligt at finde på nye komplicerede koder - og også huske dem igen. 
+      </p><p>
+      Der er hjælp at få, men der er desværre ingen vej uden om - dit password skal være unikt.</p>
       <a href="#" id="nytSvar6" class="box">Forstået - videre</a>
     `;
   } else if (answer === 'nytSvar5'|| answer === 'nytSvar6' ) {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
       <h2>Du har nu skabt et sikkert password</h2>
-      <p>Og du ved hvordan du skal forholde dig i fobindelse med læk. Men hvordan husker du?</p>
-      <a href="#" id="nytSvar7" class="box">Memorere</a>
-      <a href="#" id="nytSvar8" class="box">skriver det ned</a>
-      <a href="#" id="nytSvar9" class="box">Password manager</a>
+      <p>Tillykke med det - det er både langt og kryptisk. Hvordan husker du det?</p>
+      <a href="#" id="nytSvar7" class="box">Jeg memorerer det</a>
+      <br><br>
+      <a href="#" id="nytSvar8" class="box">Jeg skriver det ned</a>
+      <br><br>
+      <a href="#" id="nytSvar9" class="box">Jeg bruger en password manager</a>
     `;
   } else if (answer === 'nytSvar7') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
-      <h2>Memorerer</h2>
-      <p>Udemærket - men hvis du føler dig overvældet kan du bruge en password manager</p>
+      <h2>Huskespillet</h2>
+      <p>Det er en udemærket strategi. Men mens du husker dit password, så husk på at du gerne skal have unikke passwords til hvert login. Hvis det er for overvældende, kan du overveje at benytte en password manager.</p>
     `;
   } else if (answer === 'nytSvar8') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
       <h2>Skriv det ned</h2>
-      <p>Udemærket - men for at opnå den bedst sikkerhed, krypter eller lås inde</p>
+      <p>Det kan være særdeles hjælpsomt at skrive sine passwords ned. Men om du gør det fysisk eller digitalt, så sørg endelig for at henholdsvis lås dem inde eller kryptere dem.</p>
     `;
   } else if (answer === 'nytSvar9') {
     // Skift spørgsmålet og svarmulighederne baseret på det valgte svar
     questionDiv.innerHTML = `
-      <h2>Password manager</h2>
-      <p>Du har nu et bådet sikkert og sikret password. </p>
+      <h2>Password manageren</h2>
+      <p>Med dine passwords sikkert gemt i en password manager kan du glemme dine passwords. Du bliver logget ind med biometrisk input - ansigtsgenkendelse eller fingeraftryk. Og hvis du skal bruge et password på en enhed der ikke er sat op med din password manager kan du blot åbne manageren på en af dine andre enheder og finde dit kartotek af adgangskoder.</p>
     `;
   } 
 
